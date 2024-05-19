6c7eea4e61963de1abb545ef70f9837a4a9c8afd
Md Abdur Rahim <98558957+marahim34@users.noreply.github.com>
2024-05-19 11:12:49 +0300

    if else condition in c++

---
+++ Module-01/condition1.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int x = 10;
+
+    if (x == 10)
+    {
+        cout << "It is " << x << " out of 10";
+    }
+    else
+    {
+        cout << "This is not 10";
+    }
+
+    return 0;
+}
