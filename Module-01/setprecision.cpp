41d8ebde340e61a40856d641b1f3509b44a3f003
Md Abdur Rahim <98558957+marahim34@users.noreply.github.com>
2024-05-19 10:53:31 +0300

    set precision

---
+++ Module-01/setprecision.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+int main()
+{
+    double a;
+    cin >> a;
+    cout << a << endl;
+    cout << fixed << setprecision(7) << a << endl;
+
+    return 0;
+}
