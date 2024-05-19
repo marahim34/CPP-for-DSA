ffe93d3550bde9cecc9d9ba696c845051cfa2efa
Md Abdur Rahim <98558957+marahim34@users.noreply.github.com>
2024-05-19 10:02:25 +0300

    input

---
+++ Module-01/input.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a, b;
+    cin >> a >> b;
+
+    cout << a << "\n"
+         << b << endl;
+
+    char c;
+    cin >> c;
+
+    cout << c << endl;
+
+    // asci
+    char d;
+    cin >> d;
+    int ascii = int(d);
+
+    cout << ascii << endl;
+}
---
+++ Module-01/output.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+// using namespace std;
+
+int main()
+{
+    // std::cout << "HELLO WORLD";
+
+    int a = 10;
+    long long int b = 100000000;
+    float c = 1.5;
+    double d = 2.33442424242;
+    char e = 'A';
+
+    std::cout << "My favorite number is " << a << "\n"
+              << b << std::endl
+              << c << " " << d << std::endl
+              << e;
+
+    return 0;
+}
