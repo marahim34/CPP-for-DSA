10a75ff8037b9a7e5f918bc8b77b9f9f7c266f34
Md Abdur Rahim <98558957+marahim34@users.noreply.github.com>
2024-05-20 08:46:58 +0300

    bits/stdc++.h

---
+++ Module-01/bits_header_file.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+// #include <iostream>
+// #include <string>
+// #include <algorithm>
+// #include <iomanip>
+// #include <utility>
+// #include <stdio.h>
+using namespace std;
+
+int main()
+{
+    cout << "Hello" << endl;
+    return 0;
+}
