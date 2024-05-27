d42eac93986e9d2a4685ec87242675c16633904a
Md Abdur Rahim <98558957+marahim34@users.noreply.github.com>
2024-05-27 11:42:40 +0300

    string constructor

---
+++ Module-06/string_constructor.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    // string s = "Hello World";
+    // string s("Hello World");
+
+    // string s("Hello World", 5 );
+    string a = "Hello World";
+    string s(a, 0, 5);
+
+    string x(5, 'x');
+    cout << s << endl;
+    cout << x << endl;
+    return 0;
+}
