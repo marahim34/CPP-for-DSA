7820a7623901171b4f2a0bf358e5b6c312feb024
Md Abdur Rahim <98558957+marahim34@users.noreply.github.com>
2024-05-28 08:18:37 +0300

    sorting

---
+++ Module-06/sort_string.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    string s;
+    getline(cin, s);
+
+    sort(s.begin(), s.end());
+    getchar();
+
+    cout << s << endl;
+
+    return 0;
+}
