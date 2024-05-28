c93753e0c8982c25e2c510403b31d60619e5990d
Md Abdur Rahim <98558957+marahim34@users.noreply.github.com>
2024-05-29 00:45:28 +0300

    this keyword in c++

---
+++ Module-06/this_keyword.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Person
+{
+public:
+    string name;
+    int age;
+    Person(string name, int age)
+    {
+        this->name = name;
+        this->age = age;
+    }
+
+    void hello()
+    {
+        cout << "Hello" << endl;
+    }
+};
+
+main()
+{
+    Person rakib("Rakib Mia", 24);
+
+    cout << rakib.name << " " << rakib.age << endl;
+
+    return 0;
+}
